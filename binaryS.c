10cd0f33983a0511d24a2ff06a5dcf0b23de6607
Balieiro <balieiro1396@gmail.com>
2020-11-05 12:10:17 -0300

    sem usar o m como parametro

--- binaryS.c
+++ binaryS.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 
 
-int binaryS(int arr[], int v, int l, int m, int r)
+int binaryS(int arr[], int v, int l, int r)
 {
-    if (l < r) {
-        if (v == arr[m] || v == arr[l] || v == arr[r])
-            return 0;
+    if (l <= r) {
+		int m = l + (r - l)/2;
+        if (v == arr[m]) 
+            return m;
 
-        if (v < arr[m]) {
-            r = m;
-            m = l + (r - l) / 2;
-            return binarysrc(arr, v, l, m, r);
-        }
-        if (v > arr[m]) {
-            l = m+1;
-            m = l + (r - l) / 2;
-            return binarysrc(arr, v, l, m, r);
-        }
+        if (v < arr[m]) 
+            return binaryS(arr, v, l, (m - 1));
+
+		return binaryS(arr, v, (m + 1), r);
     }
-	return 1;
+
+	return -1;
 }
 
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7,8};
+	int n = sizeof(a)/sizeof(int);
+	int x = 5;
+	int index = binaryS(a, x, 0, (n-1));
+
+	printf("index: %d\n", index);
+
+	return 0;
+}
