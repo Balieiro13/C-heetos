304e41a4e1d3aba8647a6506fba2fca7fe876d54
André Balieiro <balieiro1396@gmail.com>
2020-11-16 02:01:10 -0300

    mdc(10, m % 10) resolve

--- CP/BookReading.c
+++ CP/BookReading.c
@@ -4,9 +4,30 @@
 
 ll int solve(ll int n, ll int m)
 {
-    ll int k, r = n - (n % m);
-    k = (m + r)*(r/m) / 2;
-    return k;
+    ll int  k, j, l = (n - n % m) / m, ans = 0;
+    int i = 1;
+    j = m % 10;
+    if (j == 0)
+        return 0;
+        if (j == 1 || j == 3 || j == 7 || j == 9){
+        ans = 45;
+        i = 10;
+    }
+    if (j == 2 || j == 4 || j == 6 || j == 8){
+        ans = 20;
+        i = 5;
+    }
+    if (j == 5){
+        ans = 5;
+        i = 2;
+    }
+    ans = ans * (l / i);
+    k = l % i;
+    for (int a = 1 ; a <= k; a++){
+        ans += ((m * a) % 10);
+    }
+    
+    return ans;
 }
 
 int main()
